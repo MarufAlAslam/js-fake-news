bed8c494e76ab453e685dd78794c56ce02d49632
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-09-03 02:32:41 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -1,20 +1,46 @@
+
+const putLength = (len) => {
+    const numOfNews = document.getElementById("numOfNews");
+    numOfNews.innerText = len;
+}
+const displayNews = news => {
+    console.log(news.data);
+    const len = news.data.length;
+    putLength(len);
+}
+
+const loadNewsDetails = (url) => {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setTimeout(() => {
+            const spinner = document.getElementById("spinner");
+            spinner.style.display = "none";
+            displayNews(data)
+        }, 1000))
+        .catch(error => console.log(error));
+
+}
+
+
 const categoryListener = (id) => {
     const categoryBtn = document.getElementsByClassName("category");
     const clickedBtn = document.getElementsByClassName("btn" + id);
+
+    const activeText = clickedBtn[0].innerText;
+    // put category name into the breadcrumb
+    const categoryName = document.getElementById("category-name");
+    categoryName.innerText = activeText;
     for (const btn of categoryBtn) {
+        const spinner = document.getElementById("spinner");
+        spinner.style.display = "block";
         if (btn.classList.contains("active")) {
             btn.classList.remove('active');
         }
         clickedBtn[0].classList.add("active");
 
-
-
-        // put category name into the breadcrumb
-        const categoryName = document.getElementById("category-name");
-        categoryName.innerText = btn.innerText;
     }
     const newsUrl = `https://openapi.programming-hero.com/api/news/category/${id}`;
-    console.log(newsUrl);
+    loadNewsDetails(newsUrl);
 }
 
 
